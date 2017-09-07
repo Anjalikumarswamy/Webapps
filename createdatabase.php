21e58e6f417f4354455ed06080b78dbdcfa62a1e
Anjalikumarswamy <anjalikumarswamy@gmail.com>
2017-09-07 19:39:36 +0530

    Add files via upload

---
+++ createdatabase.php
@@ -0,0 +1,22 @@
+<?php
+ // DB connection info
+ $host = "tcp:dbphp.database.windows.net,1433";
+ $user = "anjali@dbphp";
+ $pwd = "2bv13cs013#";
+ $db = "Dbphp";
+ try{
+     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+     $sql = "CREATE TABLE registration_tbl(
+     id INT NOT NULL IDENTITY(1,1) 
+     PRIMARY KEY(id),
+     name VARCHAR(30),
+     email VARCHAR(30),
+     date DATE)";
+     $conn->query($sql);
+ }
+ catch(Exception $e){
+     die(print_r($e));
+ }
+ echo "<h3>Table created.</h3>";
+ ?>
